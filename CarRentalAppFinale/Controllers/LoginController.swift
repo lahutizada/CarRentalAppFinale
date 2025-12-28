1d19869b0ab14dec4fd5339661f10e0d0c6b2eaa
lahutizada <lahutizada@gmail.com>
2025-12-28 21:59:13 +0400

    loginController

--- CarRentalAppFinale/Controllers/LoginController.swift
+++ CarRentalAppFinale/Controllers/LoginController.swift
@@ -8,11 +8,34 @@
 import UIKit
 
 class LoginController: UIViewController {
-
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var forgotPasswordButton: UIButton!
+    @IBOutlet weak var loginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        configureUI()
+        
     }
     
-
+    func configureUI() {
+        emailTextField.layer.cornerRadius = 35
+        emailTextField.clipsToBounds = true
+        passwordTextField.layer.cornerRadius = 35
+        passwordTextField.clipsToBounds = true
+        loginButton.layer.cornerRadius = 35
+        loginButton.clipsToBounds = true
+        loginButton.tintColor = .loginBlack
+        
+    }
+    
+    
+    @IBAction func forgotPasswordButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func loginButtonTapped(_ sender: Any) {
+    }
+    
 }
