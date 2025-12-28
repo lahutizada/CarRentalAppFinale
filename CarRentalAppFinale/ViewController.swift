8648e22131eb8b531aa1e424e24e99ecb67603ff
lahutizada <lahutizada@gmail.com>
2025-12-28 21:42:58 +0400

    Initial Commit

---
+++ CarRentalAppFinale/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CarRentalAppFinale
+//
+//  Created by Ruslan Lahutizada on 28.12.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
